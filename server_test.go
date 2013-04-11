af011881865edb56df8e1339aded8b436ce3a74e
wwwouter <wouter@wercker.com>
2013-04-11 13:35:41 +0200

    first

---
+++ server_test.go
@@ -0,0 +1,25 @@
+package server
+
+import ("testing")
+
+
+type MockPusherEndpoint struct{
+	post(body []byte)(data *string, err error)
+	postResult string
+}
+
+func (this MockPusherEndpoint) post(body []byte)(data *string, err error){
+	return this.postResult, nil
+}
+
+func TestTesting(t *testing.T) {
+	endpoint := MockPusherEndpoint{postResult = "{iets:1}"}
+	pusher := Pusher{poster = endpoint}
+	result := pusher.Trigger("channel", "", "dus")
+
+	if result != "ok" {
+		t.Error("Das nie goe")
+	}
+}
+
+
